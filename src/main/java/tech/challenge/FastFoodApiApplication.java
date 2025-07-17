75935db744e008cefa7f374b665d375a9d5289e1
Nathan Cordeiro <nathancribeiro13@hotmail.com>
2025-07-17 12:12:09 -0300

    First commit

---
+++ src/main/java/tech/challenge/FastFoodApiApplication.java
@@ -0,0 +1,13 @@
+package tech.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastFoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastFoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/challenge/FastFoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastFoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
