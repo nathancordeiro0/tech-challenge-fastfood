0088eafb6dcbad1e7bbb78bed4409b19087374c3
Nathan Cordeiro <nathancribeiro13@hotmail.com>
2025-08-08 14:02:04 -0300

    feat: add local Kubernetes setup using Minikube

---
+++ initial-script.ps1
@@ -0,0 +1,10 @@
+Write-Output "🛠️ Iniciando o Minikube..."
+minikube start
+
+Write-Output "☸️ Aplicando arquivos do Kubernetes..."
+
+$yamlPath = "C:\Users\natha\github.com\fast-food\k8s"
+
+kubectl apply -f $yamlPath
+
+Write-Output "✅ Automação concluída em $(Get-Date)"
---
+++ initial-script.sh
@@ -0,0 +1,10 @@
+echo "🛠️ Iniciando o Minikube..."
+minikube start
+
+echo "☸️ Aplicando arquivos do Kubernetes..."
+
+yamlPath="$HOME/github.com/fast-food/k8s"
+
+kubectl apply -f "$yamlPath"
+
+echo "✅ Automação concluída em $(date)"
