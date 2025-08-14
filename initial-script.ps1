4e7b9186472d6584572a646efb992a2b9d9d2563
Nathan Cordeiro <nathancribeiro13@hotmail.com>
2025-08-14 17:08:37 -0300

    feat: update local Kubernetes with ingress and ngrok

--- initial-script.ps1
+++ initial-script.ps1
@@ -1,10 +1,13 @@
-Write-Output "🛠️ Iniciando o Minikube..."
+Write-Output "Iniciando o Minikube..."
 minikube start
 
-Write-Output "☸️ Aplicando arquivos do Kubernetes..."
+Write-Output "Instalando Ingress Controller (nginx)..."
+minikube addons enable ingress
+
+Write-Output "Aplicando arquivos do Kubernetes..."
 
 $yamlPath = "C:\Users\natha\github.com\fast-food\k8s"
 
 kubectl apply -f $yamlPath
 
-Write-Output "✅ Automação concluída em $(Get-Date)"
+Write-Output "Automação concluída em $(Get-Date)"
